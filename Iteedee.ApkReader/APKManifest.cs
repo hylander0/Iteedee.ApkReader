5c48dfbe09fce9669e4bb54d925c62f26f88d5c4
vassav <0vassav0@gmail.com>
2017-09-04 18:11:35 +0300

    Fix correct parse UTF16 text

--- Iteedee.ApkReader/APKManifest.cs
+++ Iteedee.ApkReader/APKManifest.cs
@@ -201,15 +201,15 @@ namespace Iteedee.ApkReader
         // is followed by that number of 16 bit (Unicode) chars.
         public String compXmlStringAt(byte[] arr, int strOff)
         {
-            int strLen = arr[strOff + 1] << 8 & 0xff00 | arr[strOff] & 0xff;
+            int strLen = (arr[strOff + 1] << 8 & 0xff00 | arr[strOff] & 0xff) * 2;
             byte[] chars = new byte[strLen];
             for (int ii = 0; ii < strLen; ii++)
             {
-                chars[ii] = arr[strOff + 2 + ii * 2];
+                chars[ii] = arr[strOff + 2 + ii];
             }
 
 
-            return System.Text.Encoding.UTF8.GetString(chars);  // Hack, just use 8 byte chars
+            return System.Text.Encoding.GetEncoding("UTF-16").GetString(chars); 
         } // end of compXmlStringAt
 
 
